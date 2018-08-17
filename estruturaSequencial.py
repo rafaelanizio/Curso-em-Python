d2de7bf13125c1c88d2ea892b9ede6d2535f9577
rafael.goncalves <rafael.anizio38@gmail.com>
2018-08-17 13:14:30 -0300

    innitial commit

---
+++ estruturaSequencial.py
@@ -0,0 +1,63 @@
+# https://wiki.python.org.br/ListaDeExercicios
+# https://wiki.python.org.br/EstruturaSequencial
+
+# Faça um Programa que mostre a mensagem "Alo mundo" na tela. /*
+"""
+print("Alo mundo")
+"""
+# faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+from builtins import int
+
+"""num = input("Digite um numero : ")
+print('Seu numero é :',num)
+ """
+# Faça um Programa que peça dois números e imprima a soma.
+"""num1 = input("Digite o primeiro numero :")
+num2 = input("Digite o segundo numero :")
+soma = int (num1)+int(num2)
+print(soma)
+"""
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+"""nota1 = input('Digite a nota : ')
+nota2 = input('Digite a nota : ')
+nota3 = input('Digite a nota : ')
+nota4 = input('Digite a nota : ')
+media =  (int (nota1)+int(nota2)+int(nota3)+int (nota4))/4
+print ('A média desse aluno é: ', media)"""
+
+#Faça um Programa que converta metros para centímetros.
+"""metros = 100
+cent = input('Digite a quantidade de metros para ser convertida em cm : ')
+
+cm = int(cent) * int (metros)
+print(cent ,'metros  tem ',cm,'cm')"""
+
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+# Formula A = pi  x  r^2
+"""pi= 3.14
+num = float(input('Digite o raio : '))
+raio = num ** 2
+area = pi*raio
+print(area)
+"""
+# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+"""
+altura = float(input('Digite o valor do lado/altura  do quadrado: '))
+area =  altura**2
+print('A  area do quadrado é : ',area)
+areaQuadrado = area ** 2
+print('O dobro dessa área é : ', areaQuadrado)"""
+
+#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+#Calcule e mostre o total do seu salário no referido mês.
+"""horaTrabalho = float(input('Quanto voce recebe por hora trabalhada? : R$ '))
+horaMes =  int(input('Quantas horas você trabalha no mês ? :'))
+salario  = horaTrabalho * horaMes
+print('Seu salário é de R$',salario)"""
+
+
+
+
+
+
